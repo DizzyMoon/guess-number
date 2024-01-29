a77859b3a07f6a0f62fd1e86c06d80c3ce603ebd
DizzyMoon <mikkel.simons1@gmail.com>
2024-01-29 13:11:33 +0100

    Created template functions

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ document.addEventListener("load", start())
 function start() {
     console.log("Javascript is running");
 }
+
+function generateRandomNumber() {
+
+}
+
+function receiveGuess() {
+
+}
+
+function compareGuess() {
+
+}
+
+function isCorrect() {
+
+}
+
+function guessTooLow() {
+
+}
+
+function guessToHigh() {
+    
+}
